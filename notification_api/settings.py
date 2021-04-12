1dc8be6eb2e3fa722808b14a3080b822882a29f9
Oleg Smirnov <smirnov.nameless@ya.ru>
2021-04-12 20:29:03 +0300

    Create rabbit service

--- notification_api/settings.py
+++ notification_api/settings.py
@@ -1,1 +1,4 @@
-RABBIT_URL = "amqp://guest:guest@127.0.0.1:/"
+from os import environ as env
+
+RABBIT_URL = env.get("RABBIT_URL", "amqp://guest:guest@127.0.0.1:/")
+EXCHANGE_NAME = env.get("EXCHANGE_NAME", "notifications")
