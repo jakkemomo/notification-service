9b21494250b310ebc946a901ec849f537922af18
Oleg Smirnov <smirnov.nameless@ya.ru>
2021-04-16 12:02:08 +0300

    Add user authorization

--- notification_api/src/api/v1/user_notice.py
+++ notification_api/src/api/v1/user_notice.py
@@ -5,6 +5,7 @@ from pydantic import parse_obj_as
 
 from notification_api.src.exceptions import DocAlreadyExists, DocNotFound
 from notification_api.src.models.api import ExcludedNoticeOut
+from notification_api.src.services.auth import AuthorizedUser, get_user
 from notification_api.src.services.user_notice import (
     UserNoticeService,
     get_user_notice_service,
@@ -16,35 +17,44 @@ user_notice_api = APIRouter()
 @user_notice_api.get("/notice")
 async def get_user_notice_list(
     user_notice_service: UserNoticeService = Depends(get_user_notice_service),
+    current_user: AuthorizedUser = Depends(get_user),
 ) -> List[ExcludedNoticeOut]:
-    # TODO: User authorization
-    user_id = "user-12345"
+    if not current_user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    user_id = current_user.id
+
     excluded_notices = await user_notice_service.get_excluded_notices(user_id)
     return parse_obj_as(List[ExcludedNoticeOut], excluded_notices)
 
 
-@user_notice_api.post("/notice/{notice_type}")
-async def create_notice(
-    notice_type: str,
+@user_notice_api.post("/notice/{notice_id}")
+async def activate_notice_type(
+    notice_id: str,
     user_notice_service: UserNoticeService = Depends(get_user_notice_service),
+    current_user: AuthorizedUser = Depends(get_user),
 ):
-    # TODO: User authorization
-    user_id = "user-12345"
+    if not current_user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    user_id = current_user.id
+
     try:
-        await user_notice_service.activate(user_id, notice_type)
+        await user_notice_service.activate(user_id, notice_id)
     except DocNotFound:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
 
 
-@user_notice_api.delete("/notice/{notice_type}")
-async def delete_notice(
-    notice_type: str,
+@user_notice_api.delete("/notice/{notice_id}")
+async def deactivate_notice_type(
+    notice_id: str,
     user_notice_service: UserNoticeService = Depends(get_user_notice_service),
+    current_user: AuthorizedUser = Depends(get_user),
 ):
-    # TODO: User authorization
-    user_id = "user-12345"
+    if not current_user:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+    user_id = current_user.id
+
     try:
-        await user_notice_service.deactivate(user_id, notice_type)
+        await user_notice_service.deactivate(user_id, notice_id)
     except DocNotFound:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
     except DocAlreadyExists:
--- notification_api/src/settings.py
+++ notification_api/src/settings.py
@@ -2,3 +2,7 @@ from os import environ as env
 
 MONGO_URI = env.get("MONGO_URI", "mongodb://127.0.0.1:27017/")
 MONGO_DB = env.get("MONGO_DB", "movies")
+
+AUTH_DEBUG = env.get("AUTH_DEBUG", 0)
+USER_ID_DEBUG = env.get("USER_ID_DEBUG", "user-12345")
+AUTH_URI = env.get("AUTH_URI", "http://127.0.0.1:8000/api/v1/pubkey")
